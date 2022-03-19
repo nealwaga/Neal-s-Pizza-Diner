7f1357a12241ea61c951616edfde66d00cbf8f4d
nealwaga <waganeal@gmail.com>
2022-03-19 19:05:20 +0300

    js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@ $(document).ready(function(){
         $("#form1").toggle();
     });
 });
+
+const form1 = document.querySelector('#form1');
+
+form1.addEventListener("submit", function(event){
+    event.preventDefault();
+    const fd = new FormData(form1);
+
+    let order = {}
+    let cart = [];
+
+    for (const key of fd.keys()){
+        if(fd.get(key).toString().length > 0){
+            order[key] = fd.get(key).toString();
+        }
+    }
+    if(confirm("This pizza will be added to your cart")){
+        cart.push(order);
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+});
