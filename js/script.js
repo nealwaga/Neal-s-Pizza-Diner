a88f3027ce23d9763e42d5e1031735750dff1de3
nealwaga <waganeal@gmail.com>
2022-03-20 21:29:57 +0300

    js

--- js/script.js
+++ js/script.js
@@ -13,30 +13,26 @@ form1.addEventListener("submit", function(event){
     let order = {}
     let cart = [];
 
-    for (const key of fd.keys()){
+    for (const key of fd.keys()) {
         if(fd.get(key).toString().length > 0){
             order[key] = fd.get(key).toString();
         }
     }
-    if(confirm("Your order has been added to your cart")){
+    if(confirm("Your order has been added to the cart")){
         cart.push(order);
         localStorage.setItem("cart", JSON.stringify(cart));
+        
+        console.log(cart);
     }
 });
 
-const btnViewOrder = document.querySelector("#view-order");
-btnViewOrder.addEventListener('click', function(){
-    let cart - JSON.parse(localStorage.getItem("cart"));
-    cart.forEach(element => {
-        document.querySelector("#our-order").innerHTML += '<tr>
-        <td>${element['Size']}</td>
-        <td>${element['Crust']}</td>
-        <td>${element['Quantity']}</td>
-        <td>${element['Price']}</td>
-        </tr>';
 
-    });
-    let tot = cart.reduce((sum, item) => sum + (item['price'] || 0), 0);
-    document.querySelector("#total").innerHTML = "Total" +tot.toString();
-    console.log(localStorage.getItem("cart"));
-})
+function myFunction(){
+    let text;
+    if (confirm("Would you like us to deliver your order to your place of choice? Cost of transport will be Ksh 200") == true) {
+        text = "Your order will be delivered to your location.";
+    }   else {
+        text = "Okay";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
